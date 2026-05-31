7df514de2b2e4621459420f1b469a026d59624e0
Menagadevit <menagadevi2003@gmail.com>
2026-05-31 23:06:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './HealthFirst';
+import App from './healthfirst';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
